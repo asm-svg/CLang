9188c29e95c29c294fbb8e7023b2da5ac0ab6e95
Sonakshi <talukdarsonakshi0@gmail.com>
2026-05-03 10:57:11 +0530

    Add linear search implementation in C

---
+++ linear Search.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+   int a[50];
+   int i, loc = -1, key, n;
+
+   printf("\n Enter value of n:");
+   scanf("%d",&n);
+
+   printf("\n Enter the elements:");
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+
+   printf("Enter integer value to search in array:");
+   scanf("%d", &key);
+
+   for ( i = 0; i < n; i++ )
+   {
+       if ( a[i] == key )
+       {
+           loc = i;
+           break;
+       }
+   }
+
+   if(loc != -1)
+   {
+       printf("Element found at %d", loc + 1);
+   }
+   else
+   {
+       printf("Element not found");
+   }
+
+   return 0;
+}
